4eb3078717f4154c70aca2ad1206dfd9f3e3dd18
Van Trung <vantrung@Vans-MacBook-Pro.local>
2019-10-17 16:03:47 +0700

    add test case

---
+++ FirstLib/SwiftLib.swift
@@ -0,0 +1,23 @@
+//
+//  swiftLib.swift
+//  FirstLib
+//
+//  Created by Van Trung on 10/17/19.
+//  Copyright © 2019 NVT. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- FirstLibTests/FirstLibTests.swift
+++ FirstLibTests/FirstLibTests.swift
@@ -7,21 +7,27 @@
 //
 
 import XCTest
+
 @testable import FirstLib
 
 class FirstLibTests: XCTestCase {
+    var swiftLib: SwiftyLib!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftLib = SwiftyLib()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAdd(){
+        XCTAssertEqual(swiftLib.add(a: 1, b: 2), 3)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(swiftLib.sub(a: 1, b: 2), -1)
     }
 
     func testPerformanceExample() {
