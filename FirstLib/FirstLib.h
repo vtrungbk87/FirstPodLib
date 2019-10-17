dd219d23af00b9895c534e9b229e709298b795b6
Van Trung <vantrung@Vans-MacBook-Pro.local>
2019-10-17 14:00:58 +0700

    init project

---
+++ FirstLib/FirstLib.h
@@ -0,0 +1,19 @@
+//
+//  FirstLib.h
+//  FirstLib
+//
+//  Created by Van Trung on 10/17/19.
+//  Copyright © 2019 NVT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstLib.
+FOUNDATION_EXPORT double FirstLibVersionNumber;
+
+//! Project version string for FirstLib.
+FOUNDATION_EXPORT const unsigned char FirstLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstLib/PublicHeader.h>
+
+
